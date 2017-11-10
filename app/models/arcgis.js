21710bac9fe40cffd694c3504e8514bf2f3ab34c
jeroenvalcke <valcke_jeroen@hotmail.com>
2017-11-10 20:20:02 +0100

    Rework module for new module system (reverted from commit a40f31a3faff1c58e25b0f881a12e5bfff9ccadc) (reverted from commit d8012f64cd002a102eae0167396d4cea7b5f8ede)

--- app/models/arcgis.js
+++ app/models/arcgis.js
@@ -6,29 +6,29 @@ delete mongoose.models.Arcgis;
 delete mongoose.modelSchemas.Arcgis;
 
 var ArcgisSchema = new Schema({
-    uuid: {
-        type: String,
-        default: uuid,
-        required: true
-    },
-    contentRef: {
-        type: String,
-        ref: "Content",
-        required: true
-    },
-    drawingRef: {
-        type: String,
-        required: true
-    },
-    arcgisRef: {
-        type: String,
-        required: true
-    },
-    type: {
-        type: String,
-        required: true,
-        enum: ["point", "polygon"]
-    }
+	uuid: {
+		type: String,
+		default: uuid,
+		required: true,
+	},
+	contentRef: {
+		type: String,
+		ref: "Content",
+		required: true,
+	},
+	drawingRef: {
+		type: String,
+		required: true,
+	},
+	arcgisRef: {
+		type: String,
+		required: true,
+	},
+	type: {
+		type: String,
+		required: true,
+		enum: ["point", "polygon"],
+	},
 });
 
 // Set the name of the collection
