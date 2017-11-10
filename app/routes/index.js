a40f31a3faff1c58e25b0f881a12e5bfff9ccadc
jeroenvalcke <valcke_jeroen@hotmail.com>
2017-11-10 20:19:06 +0100

    Rework module for new module system

--- app/routes/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-    // Require controllers to start listeners
-    require("../controllers/create");
-    require("../controllers/update");
-    require("../controllers/remove");
-};
