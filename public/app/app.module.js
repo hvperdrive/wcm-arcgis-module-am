2a56362d6a5f12cb866e6d2a5ad375e8895022ac
Denis Valcke <denis.valcke@studiohyperdrive.be>
2021-11-03 13:57:22 +0100

    chore: bump to v1.1.12

--- public/app/app.module.js
+++ public/app/app.module.js
@@ -1,18 +1,18 @@
 "use strict";
 
-angular.module("arcgis-am_1.1.10.directives", []);
-angular.module("arcgis-am_1.1.10.factories", []);
-angular.module("arcgis-am_1.1.10.services", ["arcgis-am_1.1.10.factories"]);
-angular.module("arcgis-am_1.1.10.controllers", ["arcgis-am_1.1.10.services"]);
+angular.module("arcgis-am_1.1.12.directives", []);
+angular.module("arcgis-am_1.1.12.factories", []);
+angular.module("arcgis-am_1.1.12.services", ["arcgis-am_1.1.12.factories"]);
+angular.module("arcgis-am_1.1.12.controllers", ["arcgis-am_1.1.12.services"]);
 
 angular
-	.module("arcgis-am_1.1.10", [
+	.module("arcgis-am_1.1.12", [
 		"pelorus.services",
 
-		"arcgis-am_1.1.10.directives",
-		"arcgis-am_1.1.10.factories",
-		"arcgis-am_1.1.10.services",
-		"arcgis-am_1.1.10.controllers",
+		"arcgis-am_1.1.12.directives",
+		"arcgis-am_1.1.12.factories",
+		"arcgis-am_1.1.12.services",
+		"arcgis-am_1.1.12.controllers",
 	])
 	.run([function() {
 		console.log("Arcgis AM module is available!"); // eslint-disable-line no-console
